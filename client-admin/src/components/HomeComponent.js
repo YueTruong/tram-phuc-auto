f09fd836aabee343d7c0365fd5eb90c851a75c01
YueTruong <tienphuc01012004@gmail.com>
2025-03-18 11:02:49 +0700

    Add initial project structure with essential files and components

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="d-flex flex-column justify-content-center align-items-center vh-100 text-center">
+                <h2 className="mb-4">ADMIN HOME</h2>
+                <div className='shadow-lg p-4 rounded bg-white'>
+                    <img src="http://cliparting.com/wp-content/uploads/2018/03/animated-emoticons-2018-13.gif" width="800px" height="600px" alt="" />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
